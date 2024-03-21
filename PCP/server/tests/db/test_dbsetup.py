bedba6c16dc1dcbac90b82b51781c152594fce88
bharathi <bp6191@rit.edu>
2024-03-21 19:07:30 -0400

    DB_TestCases done

--- PCP/server/tests/db/test_dbsetup.py
+++ PCP/server/tests/db/test_dbsetup.py
@@ -2,10 +2,12 @@ import unittest
 
 from utilities.swen_344_db_utils import exec_sql_file
 
-
-# from ../src.utilities.swen_344_db_utils import exec_sql_file
-
 class PCPSetupDB(unittest.TestCase):
+    """
+    This test file contains unit tests for setting up database tables and
+    initial data required for testing purposes.
+    """
+
     # Setup test data from SQL file and also sample rows
     def test_0_setup_tables(self):
         print("Setting up test data...")
