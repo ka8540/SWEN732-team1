c1385c1ad3c7c758c5629fae4f82d8f34cd06398
bharathi <bp6191@rit.edu>
2024-03-21 11:22:23 -0400

    added two test cases for db

---
+++ PCP/server/tests/db/test_signup_db.py
@@ -0,0 +1,46 @@
+import hashlib
+import json
+import unittest
+from flask_restful import Resource
+from utilities.swen_344_db_utils import exec_get_all, exec_commit
+from ..test_utils import *
+from db.signup import list_info_items
+
+from db.signup import user_signup
+
+class SignUpDBTestCase(unittest.TestCase):
+
+    def test_a_list_info_items(self):
+        printing_response = list_info_items()
+        print(printing_response)
+
+    def test_b_new_user_registration(self):
+        # Input data
+        user_data = {
+            'firstname': 'bharathi',
+            'lastname': 'pandurangan',
+            'username': 'bp6191',
+            'password': 'Secret123!',
+            'email': 'bp6191@rit.edu'
+        }
+        response, status_code = user_signup(**user_data)
+        self.assertEqual(status_code, 200)
+        self.assertEqual(response['message'], 'User registered successfully')
+
+    def test_c_existing_user(self):
+        # Attempt to register with an existing username
+        userdata = {
+            'firstname': 'bharathi',
+            'lastname': 'pandurangan',
+            'username': 'bp6191',
+            'password': 'Secret123!',
+            'email': 'bp6191@example.com'
+        }
+        response, status_code = user_signup(**userdata)
+        self.assertEqual(status_code, 409)
+        self.assertEqual(response['message'], 'User already exists')
+
+        # Verify that the existing user is not overwritten or duplicated
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ PCP/server/tests/db/test_userdetails_db.py
@@ -0,0 +1,24 @@
+import unittest
+
+from db.user_details import list_user_detail, verify_session_key
+
+class user_details(unittest.TestCase):
+    def test_a_existing_user(self):
+        # Testing with the user 'bp6191' which exists in the database
+        expected_user = {
+            'firstname': 'bharathi',
+            'lastname': 'pandurangan',
+            'username': 'bp6191',
+            'email': 'bp6191@rit.edu'
+        }
+        actual_users = list_user_detail('bp6191')
+        self.assertEqual(actual_users, [expected_user])
+
+    def test_non_existing_user(self):
+        # Testing with 'bhara'. The username which does not exist in the database
+        actual_users = list_user_detail('bhara')
+        self.assertEqual(actual_users, []) # returns an empty set when user doesnt exists in the database
+
+
+if __name__ == '__main__':
+    unittest.main()
