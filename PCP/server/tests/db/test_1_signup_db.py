9ece552895b18477f6b360f90993222c9bfeee4c
la3679 <143540361+la3679@users.noreply.github.com>
2024-03-26 13:29:46 -0400

    Update test_1_signup_db.py

--- PCP/server/tests/db/test_1_signup_db.py
+++ PCP/server/tests/db/test_1_signup_db.py
@@ -4,8 +4,8 @@ import unittest
 from flask_restful import Resource
 from utilities.swen_344_db_utils import exec_get_all, exec_commit
 from ..test_utils import *
-from db.signup import list_info_items
-from db.signup import user_signup
+from src.db.signup import list_info_items
+from src.db.signup import user_signup
 
 
 class SignUpDBTestCase(unittest.TestCase):
