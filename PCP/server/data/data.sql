c72a0196c119154bbcd360972aa960b4fe6890f7
ka8540 <ka8540@g.rit.edu>
2024-04-18 14:05:30 -0400

    edit backend and frontend

--- PCP/server/data/data.sql
+++ PCP/server/data/data.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS Retailers CASCADE;
 DROP TABLE IF EXISTS ProductCategories CASCADE;
 DROP TABLE IF EXISTS user_authentication CASCADE;
 DROP TABLE IF EXISTS Currency CASCADE;
-DROP Table IF EXISTS Cart;
+DROP TABLE IF EXISTS Cart;
 
 -- Using VARCHAR as it is the standard string type in PostgreSQL
 
